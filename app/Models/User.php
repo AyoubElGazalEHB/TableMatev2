0ac0e895ba8d99396a2732c6859624a69cf876a0
Unixere <ayoub.el.gazal@student.ehb.be>
2025-08-05 03:15:57 +0200

    Role model created and user model updated

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,4 +38,19 @@ class User extends Authenticatable
     {
         return $this->hasMany(Faq_Items::class, 'user_id');
     }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'user_roles');
+    }
+
+    public function hasRole($roleName)
+    {
+        return $this->roles()->where('name', $roleName)->exists();
+    }
+
+    public function isAdmin()
+    {
+        return $this->hasRole('Admin') || $this->typeUser == '1';
+    }
 }
